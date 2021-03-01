d12ef57f6a42720e70747511ee4016a1bf68b3a0
ady4k <ady4k@users.noreply.github.com>
2021-03-01 13:34:33 +0200

    supraincarcare

--- Supraincarcare.cpp
+++ Supraincarcare.cpp
@@ -2,12 +2,19 @@
 // Created by Ady on 01/03/2021.
 //
 
-#include "Persoana.cpp"
+#include "Persoana.h"
+using namespace std;
 
 istream& operator>>(istream &in, Persoana &persoana) {
-
+    string nume_;
+    int anNastere_;
+    char sex_;
+    in >> nume_ >> anNastere_ >> sex_;
+    persoana.seteazaValori(nume_, anNastere_, sex_);
+    return in;
 }
 
 ostream& operator<<(ostream &out, Persoana &persoana) {
-
+    out << persoana.getNume() << ' ' << persoana.getAnNastere() << ' ' << persoana.getSex() << '\n';
+    return out;
 }
